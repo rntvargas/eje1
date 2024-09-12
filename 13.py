43e769d303574487e806f2ae582048864ed2a6c4
rntvargas <56089909+rntvargas@users.noreply.github.com>
2024-09-11 23:10:02 -0500

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,4 @@
+frase = input("Introduce una frase: ")  
+vocal = input("Introduce una vocal: ")  
+frase_modificada = frase.replace(vocal, vocal.upper())  # Cambiamos la vocal por mayúsculas
+print(frase_modificada)
